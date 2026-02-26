4d2c0ca8badf0ac555a459de52b2d2401ffe0cf2
Chris Bachhuber <chris.bachhuber@eu.socionext.com>
2026-02-26 17:10:10 +0100

    Complete test cases (for now)

--- tests/test_check_max_one_sentence_per_line.py
+++ tests/test_check_max_one_sentence_per_line.py
@@ -12,49 +12,55 @@ if TYPE_CHECKING:
 
 
 @pytest.mark.parametrize(
-    ("input", "expected_return", "expected_output"),
+    ("input", "expected_output", "expected_return"),
     [
         pytest.param(
-            "This. Must match.",
+            "This. Splits.",
+            "This.\nSplits.",
             1,
-            "This.\nMust match.",
             id="fixes_two_sentences",
         ),
         pytest.param(
             "One long, long sentence with many words.",
-            0,
             "One long, long sentence with many words.",
+            0,
             id="no_issues",
         ),
         pytest.param(
             "This is a sentence. This is another sentence. And another.",
-            1,
             "This is a sentence.\nThis is another sentence.\nAnd another.",
+            1,
             id="fixes_three_sentences",
         ),
         pytest.param(
-            "1. this is an enumeration\n2. this is another item",
+            "1. this is an enumeration\n2. This is another item",
+            "1. this is an enumeration\n2. This is another item",
             0,
-            "1. this is an enumeration\n2. this is another item",
             id="does_not_split_enumeration",
         ),
         pytest.param(
             "This is a sentence! Is this another? Yes.",
-            1,
             "This is a sentence!\nIs this another?\nYes.",
+            1,
             id="fixes_sentences_with_different_punctuation",
         ),
         pytest.param(
             "This is a sentence.  This is another.",
-            1,
             "This is a sentence.\nThis is another.",
+            1,
             id="fixes_sentences_with_multiple_spaces",
         ),
         pytest.param(
-            "Do not split special abbreviations like e.g. or i.e. or Dr. in the middle of a sentence.",
+            "Do not split abbreviations like e.g. or i.e. or Dr. in the middle of a sentence.",
+            "Do not split abbreviations like e.g. or i.e. or Dr. in the middle of a sentence.",
             0,
-            "Do not split special abbreviations like e.g. or i.e. or Dr. in the middle of a sentence.",
             id="does_not_split_sentences_with_predefined_abbreviations",
+        ),
+        pytest.param(
+            "Limitation: split abbreviations followed by a space and a capital letter such as Dr. Hyde.",
+            "Limitation: split abbreviations followed by a space and a capital letter such as Dr.\nHyde.",
+            1,
+            id="does_split_sentences_with_abbreviations_followed_by_a_space_and_capital_letter",
         ),
     ],
 )
@@ -77,9 +83,9 @@ def test_main(
 def test_multiple_files_one_failing(fs: FakeFilesystem) -> None:
     file_a = Path("Repo/file_a.md")
     file_b = Path("Repo/file_b.md")
-    fs.create_file(file_a, contents="This. Must match.")
+    fs.create_file(file_a, contents="This. Splits.")
     fs.create_file(file_b, contents="One sentence.")
 
     assert main([str(file_a), str(file_b)]) == 1
-    assert file_a.read_text() == "This.\nMust match."
+    assert file_a.read_text() == "This.\nSplits."
     assert file_b.read_text() == "One sentence."
