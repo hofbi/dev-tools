7af69925e999804b1da110bd6ae83c3a8d16bd19
Chris Bachhuber <chris.bachhuber@eu.socionext.com>
2026-03-10 13:19:20 +0100

    Fix: tables with multiple columns

--- tests/test_check_max_one_sentence_per_line.py
+++ tests/test_check_max_one_sentence_per_line.py
@@ -81,10 +81,16 @@ if TYPE_CHECKING:
             id="does_not_split_python3_code_blocks",
         ),
         pytest.param(
-            "| Two sentencens. In a table cell. |",
-            "| Two sentencens. In a table cell. |",
+            "| Foo | Two sentencens. In a table cell. |",
+            "| Foo | Two sentencens. In a table cell. |",
             0,
             id="does_not_split_sentences_in_table_cells",
+        ),
+        pytest.param(
+            "| Foo |\nTwo sentencens. Between tables.\n| Bar |",
+            "| Foo |\nTwo sentencens.\nBetween tables.\n| Bar |",
+            1,
+            id="splits_sentences_between_tables",
         ),
     ],
 )
