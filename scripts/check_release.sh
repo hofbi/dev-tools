44f4f0ffea3e1377be20db222662196bbd0242f1
Markus Hofbauer <markus.hofbauer@flyzipline.com>
2026-03-31 12:04:35 +0200

    fix release script with dash based package names und underscrore paths

--- scripts/check_release.sh
+++ scripts/check_release.sh
@@ -6,6 +6,17 @@ SEMVER_REGEX='[0-9]+\.[0-9]+\.[0-9]+'
 ROOT_TAG_REGEX="^${SEMVER_REGEX}$"
 SUBPACKAGE_TAG_REGEX="^([a-zA-Z0-9_-]+)-(${SEMVER_REGEX})$"
 
+package_name_to_directory() {
+  local package_name="${1:-}"
+
+  if [[ -z "${package_name}" ]]; then
+    echo "Usage: package_name_to_directory <package-name>" >&2
+    return 1
+  fi
+
+  printf '%s\n' "${package_name//-/_}"
+}
+
 parse_release_tag() {
   local tag="${1:-}"
 
@@ -21,7 +32,7 @@ parse_release_tag() {
   if [[ "${tag}" =~ ${SUBPACKAGE_TAG_REGEX} ]]; then
     RELEASE_PACKAGE_NAME="${BASH_REMATCH[1]}"
     RELEASE_VERSION="${BASH_REMATCH[2]}"
-    RELEASE_PYPROJECT_PATH="packages/${RELEASE_PACKAGE_NAME}/pyproject.toml"
+    RELEASE_PYPROJECT_PATH="packages/$(package_name_to_directory "${RELEASE_PACKAGE_NAME}")/pyproject.toml"
   elif [[ ! "${tag}" =~ ${ROOT_TAG_REGEX} ]]; then
     echo "Version must be '<major>.<minor>.<patch>' or '<package>-<major>.<minor>.<patch>'" >&2
     return 1
