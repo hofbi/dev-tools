382159553a22c07e00bea403108579cc027d5d3c
Chris Bachhuber <cbachhuber89@gmail.com>
2026-03-31 10:26:30 +0200

    DAMP in tests

--- tests/test_check_ownership.py
+++ tests/test_check_ownership.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 import pytest
-from whoowns.ownership_utils import OwnerShipEntry, get_codeowners_path
+from whoowns.ownership_utils import OwnerShipEntry
 
 from dev_tools.check_ownership import (
     ReturnCode,
@@ -28,7 +28,7 @@ def repo_dir() -> Path:
 
 @pytest.fixture
 def codeowners(repo_dir: Path) -> Path:
-    return get_codeowners_path(repo_dir)
+    return repo_dir / ".github" / "CODEOWNERS"
 
 
 @pytest.mark.parametrize(
